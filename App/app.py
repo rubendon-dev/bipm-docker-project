8f1996a75ddb6e0788ca8169b62c266a196ace9c
rubendon-dev <ruben-donkers@hotmail.com>
2026-06-03 23:04:54 +0200

    Add Redis database, update Flask app with hit counter and docker-compose

--- App/app.py
+++ App/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return 'Hello BIPM!'
+    count = redis.incr('hits')
+    return f'Hello BIPM! I have been visited {count} times.'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=80)
